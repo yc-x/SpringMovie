ab68dcf11a8764e19a56be77e3059c99a907b39e
yc-x <34796396+yc-x@users.noreply.github.com>
2023-05-04 17:08:53 -0700

    First Commit

---
+++ src/test/java/dev/example/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.example.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
